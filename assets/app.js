7187bba71e351c92f44082101252fe57a31ed447
Diego Bonilla <drbv27@gmail.com>
2021-05-10 07:41:25 -0500

    First Commit

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+/*Ejercicios Basicos JS Nivel 1
+    9.Programa una función que obtenga un numero aleatorio entre 501 y 600.
+    10.Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro).Ejemplo: miFuncion(2002) devolverá true.
+    11.Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n).Ejemplo: miFuncion(5) devolverá 120.
+*/
+
+//Soluciones:
+
+//--------------------------------------------------------------------------------------------
+
+//E9S1.Solucion Simple:
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 9:")
+
+
+//--------------------------------------------------------------------------------------------
+
+//E10S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 10:")
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//E11S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 11:")
